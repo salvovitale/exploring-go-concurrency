d6106d371d4cbc28e987cf569f9c586010e060ba
Salvatore Vitale <salvatore.vitale@ing.com>
2022-04-29 14:55:32 +0200

    using unmanaged goroutines

--- cmd/main.go
+++ cmd/main.go
@@ -14,20 +14,30 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		if b, ok := queryCache(id); ok {
-			fmt.Println("from cache")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println("from database")
-			cache[id] = b
-			fmt.Println(b)
-			continue
-		}
-		fmt.Printf("Book not found id: '%v'", id)
+		go func(id int) {
+			if b, ok := queryCache(id); ok {
+				fmt.Println("from cache")
+				fmt.Println(b)
+			}
+		}(id)
+
+		go func(id int) {
+			if b, ok := queryDatabase(id); ok {
+				fmt.Println("from database")
+				cache[id] = b
+				fmt.Println(b)
+			}
+		}(id)
+		// fmt.Printf("Book not found id: '%v'", id)
+		// if we remove the following sleep call below, nothing will be printed because
+		// the go-routine will be schedule but the main thread will be done before they could do something.
 		time.Sleep(150 * time.Millisecond)
 	}
+	// with this following time sleep we will ge the complete output, given the fact that we are
+	// in control of the actual time spent in the underlying operation.
+	// However using sleeps call does not bring
+	// us a long way. There should be a better way of managing go-routines.
+	time.Sleep(2 * time.Second)
 }
 
 func queryCache(id int) (database.Book, bool) {
