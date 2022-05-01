d7df71223c576b55c9f9e1099d0d2efc28775a85
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-01 22:31:45 +0200

    channels are blocking operation

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
 	ch := make(chan int)
 
-	wg.Add(2)
-	// these 2 goroutine are completely decoupled from each other
-	// the only dependency is the channel.
-	go func(wg *sync.WaitGroup, ch chan int) {
-		ch <- 42
-		wg.Done()
-	}(wg, ch)
-	go func(wg *sync.WaitGroup, ch chan int) {
-		fmt.Println(<-ch)
-		wg.Done()
-	}(wg, ch)
+	// Channels are blocking by default.
+	// In this example channel is blocking until a message is sent.
+	// fmt.Println(<-ch)
+	// ch <- 42
 
-	wg.Wait()
+	// in this case channel is blocking until there is a receiver.
+	ch <- 42
+	fmt.Println(<-ch)
+
+	// thats why channels work in the context of goroutines.
 }
