6895e797ebef79614e6836f9ae06a3a06d75dbc1
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-01 22:54:47 +0200

    send-only and receiving-only channels

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
@@ -13,16 +12,12 @@ func main() {
 
 	// you do not want that goroutine can be both sending and receiving but in general they should only receive or send.
 	// thats why is a good practice to mark them as sending only or receiving only.
-	go func(wg *sync.WaitGroup, ch chan int) {
+	go func(wg *sync.WaitGroup, ch chan<- int) {
 		ch <- 42
-		// i m putting this here to avoid that i send and receive from the same goroutine
-		time.Sleep(10 * time.Millisecond)
-		fmt.Println(<-ch)
 		wg.Done()
 	}(wg, ch)
-	go func(wg *sync.WaitGroup, ch chan int) {
+	go func(wg *sync.WaitGroup, ch <-chan int) {
 		fmt.Println(<-ch)
-		ch <- 43
 		wg.Done()
 	}(wg, ch)
 
