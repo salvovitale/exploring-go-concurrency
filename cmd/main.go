9d642df5352deb845aeffd4b0c689662d30d2715
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-01 16:51:45 +0200

    using wg but having the issue of race condition

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 
 	"github.com/salvovitale/exploring-go-concurrency/internal/database"
@@ -12,35 +13,35 @@ var cache = map[int]database.Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
+	wg := &sync.WaitGroup{}
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		go func(id int) {
+		wg.Add(2) // we should add 1 for each goroutine
+		go func(id int, wg *sync.WaitGroup) {
 			if b, ok := queryCache(id); ok {
 				fmt.Println("from cache")
 				fmt.Println(b)
 			}
-		}(id)
-
-		go func(id int) {
+			wg.Done()
+		}(id, wg)
+		// wg.Add(1) we add 2 above. Thats why we do not need to add 1 here
+		go func(id int, wg *sync.WaitGroup) {
 			if b, ok := queryDatabase(id); ok {
 				fmt.Println("from database")
 				cache[id] = b
 				fmt.Println(b)
 			}
-		}(id)
-		// fmt.Printf("Book not found id: '%v'", id)
-		// if we remove the following sleep call below, nothing will be printed because
-		// the go-routine will be schedule but the main thread will be done before they could do something.
+			wg.Done()
+		}(id, wg)
+		// we cant remove this time sleep here otherwise we will generate a race condition as both cache and query to database are trying to accessing the cache.
 		time.Sleep(150 * time.Millisecond)
 	}
-	// with this following time sleep we will ge the complete output, given the fact that we are
-	// in control of the actual time spent in the underlying operation.
-	// However using sleeps call does not bring
-	// us a long way. There should be a better way of managing go-routines.
-	time.Sleep(2 * time.Second)
+	// now we can replace the time sleep with the proper tool
+	wg.Wait()
 }
 
 func queryCache(id int) (database.Book, bool) {
+	// we are reading here and we are writing at line 31 this can create a race condition.
 	b, ok := cache[id]
 	return b, ok
 }
