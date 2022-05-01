d6adc94e5445a5b70504f3ad6763deded84ab5e0
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-01 22:51:14 +0200

    all goroutine with bidirectional channels

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int, 1) // buffered channel
-	// un-buffered channel are having 0 buffering so there should always be a matching number of senders and receivers
-
+	ch := make(chan int) // bidirectional channel
 	wg.Add(2)
-	// these 2 goroutine are completely decoupled from each other
-	// the only dependency is the channel.
+
+	// you do not want that goroutine can be both sending and receiving but in general they should only receive or send.
+	// thats why is a good practice to mark them as sending only or receiving only.
 	go func(wg *sync.WaitGroup, ch chan int) {
 		ch <- 42
-		// if i add another message i will get a deadlock because there is only one receiver.
-		// to fix this we need buffered channels.
-		ch <- 3
+		// i m putting this here to avoid that i send and receive from the same goroutine
+		time.Sleep(10 * time.Millisecond)
+		fmt.Println(<-ch)
 		wg.Done()
 	}(wg, ch)
 	go func(wg *sync.WaitGroup, ch chan int) {
 		fmt.Println(<-ch)
+		ch <- 43
 		wg.Done()
 	}(wg, ch)
 
