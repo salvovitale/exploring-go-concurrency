2695227ac4b6ada22dce10afadc0ce7a1d60f46d
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-01 17:09:50 +0200

    using regular mutex works but is inefficient

--- cmd/main.go
+++ cmd/main.go
@@ -14,35 +14,38 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
+	m := &sync.Mutex{}
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		wg.Add(2) // we should add 1 for each goroutine
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := queryCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+			if b, ok := queryCache(id, m); ok {
 				fmt.Println("from cache")
 				fmt.Println(b)
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, m)
 		// wg.Add(1) we add 2 above. Thats why we do not need to add 1 here
-		go func(id int, wg *sync.WaitGroup) {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
 			if b, ok := queryDatabase(id); ok {
 				fmt.Println("from database")
+				m.Lock()
 				cache[id] = b
+				m.Unlock()
 				fmt.Println(b)
 			}
 			wg.Done()
-		}(id, wg)
-		// we cant remove this time sleep here otherwise we will generate a race condition as both cache and query to database are trying to accessing the cache.
-		time.Sleep(150 * time.Millisecond)
+		}(id, wg, m)
 	}
 	// now we can replace the time sleep with the proper tool
 	wg.Wait()
 }
 
-func queryCache(id int) (database.Book, bool) {
-	// we are reading here and we are writing at line 31 this can create a race condition.
+func queryCache(id int, m *sync.Mutex) (database.Book, bool) {
+	// locking also the reading is a bit inefficient because there is no harm in multple readings.
+	m.Lock()
 	b, ok := cache[id]
+	m.Unlock()
 	return b, ok
 }
 
