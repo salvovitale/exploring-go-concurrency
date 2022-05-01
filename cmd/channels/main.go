5db64015fc24f1d0eef4bcb3bc9e3a4b5c2d1317
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-01 22:46:14 +0200

    buffered channels

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	ch := make(chan int)
+	wg := &sync.WaitGroup{}
+	ch := make(chan int, 1) // buffered channel
+	// un-buffered channel are having 0 buffering so there should always be a matching number of senders and receivers
 
-	// Channels are blocking by default.
-	// In this example channel is blocking until a message is sent.
-	// fmt.Println(<-ch)
-	// ch <- 42
+	wg.Add(2)
+	// these 2 goroutine are completely decoupled from each other
+	// the only dependency is the channel.
+	go func(wg *sync.WaitGroup, ch chan int) {
+		ch <- 42
+		// if i add another message i will get a deadlock because there is only one receiver.
+		// to fix this we need buffered channels.
+		ch <- 3
+		wg.Done()
+	}(wg, ch)
+	go func(wg *sync.WaitGroup, ch chan int) {
+		fmt.Println(<-ch)
+		wg.Done()
+	}(wg, ch)
 
-	// in this case channel is blocking until there is a receiver.
-	ch <- 42
-	fmt.Println(<-ch)
-
-	// thats why channels work in the context of goroutines.
+	wg.Wait()
 }
