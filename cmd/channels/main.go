61ccc160bae45406f9e316991be881cde20fd61b
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-02 15:10:21 +0200

    solving the for loop deadlock issue with channels

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -12,15 +12,18 @@ func main() {
 
 	// as we know the sender and the receiver need to send receive the same number of messages.
 	// otherwise we are generating a deadlock.
+	// to solve the problem we can use the range operator on the channel in the for loop.
+	// and close the channel after we are done sending the messages.
 	go func(wg *sync.WaitGroup, ch chan<- int) {
 		for i := 0; i < 10; i++ {
 			ch <- i
 		}
+		close(ch)
 		wg.Done()
 	}(wg, ch)
 	go func(wg *sync.WaitGroup, ch <-chan int) {
-		for i := 0; i < 10; i++ {
-			fmt.Println(<-ch)
+		for msg := range ch {
+			fmt.Println(msg)
 		}
 		wg.Done()
 	}(wg, ch)
