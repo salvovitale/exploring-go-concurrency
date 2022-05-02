bf550be58b98388b64f1c40c551464cf54982c09
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-02 14:46:01 +0200

    if statement and closed channels

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -10,22 +10,17 @@ func main() {
 	ch := make(chan int) // bidirectional channel
 	wg.Add(2)
 
-	// you do not want that goroutine can be both sending and receiving but in general they should only receive or send.
-	// thats why is a good practice to mark them as sending only or receiving only.
 	go func(wg *sync.WaitGroup, ch chan<- int) {
-		ch <- 42
-		// if i close the channel and try to send again this will panic
-		// close(ch)
-		// ch <- 27
+		// ch <- 42
+		close(ch)
 		wg.Done()
 	}(wg, ch)
-	// we need to update the channel because only sending type can be closed.
-	// closing a channel always has to be on the sending side of the operation.
-	go func(wg *sync.WaitGroup, ch chan int) {
-		fmt.Println(<-ch)
-		close(ch)
-		// in this case i will get a 0
-		fmt.Println(<-ch)
+	go func(wg *sync.WaitGroup, ch <-chan int) {
+		if msg, isChannelOpen := <-ch; isChannelOpen {
+			fmt.Println(msg)
+		} else {
+			fmt.Println("Channel is closed!")
+		}
 		wg.Done()
 	}(wg, ch)
 
