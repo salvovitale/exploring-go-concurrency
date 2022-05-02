fc0d9f704ab275cfbc71cc61fef39ea84606226f
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-02 16:02:49 +0200

    fix double result of cache and db with channels

--- cmd/main.go
+++ cmd/main.go
@@ -16,28 +16,43 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	m := &sync.RWMutex{}
+	cacheCh := make(chan database.Book)
+	dbCh := make(chan database.Book)
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		wg.Add(2) // we should add 1 for each goroutine
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- database.Book) {
 			if b, ok := queryCache(id, m); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, cacheCh)
 		// wg.Add(1) we add 2 above. Thats why we do not need to add 1 here
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- database.Book) {
 			if b, ok := queryDatabase(id); ok {
-				fmt.Println("from database")
 				// this will lock other writers and readers.
 				m.Lock()
 				cache[id] = b
 				m.Unlock()
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, dbCh)
+
+		go func(cacheCh, dbCh <-chan database.Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("from cache")
+				fmt.Println(b)
+				// i need to read from dbChannel as well because otherwise the dbCh will never be drained completely. And I will get a deadlock error.
+				// basically if the result will come before from the cache the db result will be discard.
+				<-dbCh
+			case b := <-dbCh:
+				fmt.Println("from database")
+				fmt.Println(b)
+			}
+		}(cacheCh, dbCh)
+		// this time sleep should stay to simulate real call which are not simultaneous.
 		time.Sleep(150 * time.Millisecond)
 	}
 	// now we can replace the time sleep with the proper tool
