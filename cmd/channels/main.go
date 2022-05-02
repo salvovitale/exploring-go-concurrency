54f7d50a24c560afef102a6a2cc6163c7d5c1cb1
Salvatore Vitale <salvatore.vitale@ing.com>
2022-05-02 13:58:30 +0200

    closing channels

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -14,9 +14,17 @@ func main() {
 	// thats why is a good practice to mark them as sending only or receiving only.
 	go func(wg *sync.WaitGroup, ch chan<- int) {
 		ch <- 42
+		// if i close the channel and try to send again this will panic
+		// close(ch)
+		// ch <- 27
 		wg.Done()
 	}(wg, ch)
-	go func(wg *sync.WaitGroup, ch <-chan int) {
+	// we need to update the channel because only sending type can be closed.
+	// closing a channel always has to be on the sending side of the operation.
+	go func(wg *sync.WaitGroup, ch chan int) {
+		fmt.Println(<-ch)
+		close(ch)
+		// in this case i will get a 0
 		fmt.Println(<-ch)
 		wg.Done()
 	}(wg, ch)
